aaded7f1eb4d8b0b4d44fc821e10e2ae8285aa04
lollipopkit🏳️‍⚧️ <10864310+lollipopkit@users.noreply.github.com>
2025-08-22 22:49:30 +0800

    chore: downgrade riverpod

--- lib/generated/l10n/lib_l10n.dart
+++ lib/generated/l10n/lib_l10n.dart
@@ -73,7 +73,7 @@ import 'lib_l10n_zh.dart';
 /// property.
 abstract class LibLocalizations {
   LibLocalizations(String locale)
-      : localeName = intl.Intl.canonicalizedLocale(locale.toString());
+    : localeName = intl.Intl.canonicalizedLocale(locale.toString());
 
   final String localeName;
 
@@ -96,11 +96,11 @@ abstract class LibLocalizations {
   /// of delegates is preferred or required.
   static const List<LocalizationsDelegate<dynamic>> localizationsDelegates =
       <LocalizationsDelegate<dynamic>>[
-    delegate,
-    GlobalMaterialLocalizations.delegate,
-    GlobalCupertinoLocalizations.delegate,
-    GlobalWidgetsLocalizations.delegate,
-  ];
+        delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ];
 
   /// A list of this localizations delegate's supported locales.
   static const List<Locale> supportedLocales = <Locale>[
@@ -116,7 +116,7 @@ abstract class LibLocalizations {
     Locale('tr'),
     Locale('uk'),
     Locale('zh'),
-    Locale('zh', 'TW')
+    Locale('zh', 'TW'),
   ];
 
   /// No description provided for @about.
@@ -809,19 +809,19 @@ class _LibLocalizationsDelegate
 
   @override
   bool isSupported(Locale locale) => <String>[
-        'de',
-        'en',
-        'es',
-        'fr',
-        'id',
-        'ja',
-        'nl',
-        'pt',
-        'ru',
-        'tr',
-        'uk',
-        'zh'
-      ].contains(locale.languageCode);
+    'de',
+    'en',
+    'es',
+    'fr',
+    'id',
+    'ja',
+    'nl',
+    'pt',
+    'ru',
+    'tr',
+    'uk',
+    'zh',
+  ].contains(locale.languageCode);
 
   @override
   bool shouldReload(_LibLocalizationsDelegate old) => false;
@@ -869,8 +869,9 @@ LibLocalizations lookupLibLocalizations(Locale locale) {
   }
 
   throw FlutterError(
-      'LibLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
-      'an issue with the localizations generation tool. Please file an issue '
-      'on GitHub with a reproducible sample app and the gen-l10n configuration '
-      'that was used.');
+    'LibLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
+    'an issue with the localizations generation tool. Please file an issue '
+    'on GitHub with a reproducible sample app and the gen-l10n configuration '
+    'that was used.',
+  );
 }
